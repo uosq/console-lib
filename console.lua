e16aafb45b9f78bf882e2ece32ba1b709593be50
uosq <lboxwallytf2@gmail.com>
2024-09-12 20:55:47 -0300

    not much changes, just changed from calling a crap ton of functions to just create_command and then changing the values manually

--- console.lua
+++ console.lua
@@ -1,5 +1,10 @@
-local prefixes = {}
+---@class Command
+---@field required_parameters table The required parameters/arguments to run the command | example: {name = "string", players = "table"}
+---@field callback function The function that is used when calling the command | example: con create-command hellomom
+---@field description string The description of the command
 
+local prefixes = {}
+local debug = true
 local keys = {callback = "callback", required_parameters = "required_parameters"}
 
 ---@param s string
@@ -27,9 +32,11 @@ end
 
 ---@param prefix string
 ---@param name string
+---@return Command|nil
 local function create_command(prefix, name)
-    --assert(prefixes[prefix], "prefix is nil")
-    --assert(prefixes[prefix].command_list, "command_list is nil")
+    assert(prefixes[prefix], "prefix is nil")
+    assert(prefixes[prefix].command_list, "command_list is nil")
+
     prefixes[prefix].command_list[name] = {
         callback = nil,
         required_parameters = {}, -- {name = "string"},
@@ -38,10 +45,12 @@ local function create_command(prefix, name)
 
     if not prefixes[prefix].command_list[name] then
         printc(255,100,100,255, "couldn't create command %s at prefix %s", name, prefix)
+        return nil
     else
-        printc(100,255,100,255, string.format("create command %s at prefix %s", name, prefix))
+        printc(100,100,255,255, string.format("create command %s at prefix %s", name, prefix))
     end
 
+    return prefixes[prefix].command_list[name]
 end
 
 ---@param prefix string
@@ -54,7 +63,7 @@ local function destroy_command(prefix, name)
         printc(255,100,100,255, string.format("command %s at prefix %s wasn't destroyed somehow", name, prefix))
         return false
     end
-    printc(100,255,100,255, string.format("destroyed command %s at prefix %s", name, prefix))
+    printc(255,100,100,255, string.format("destroyed command %s at prefix %s", name, prefix))
     return true
 end
 
@@ -70,7 +79,7 @@ local function change_command(prefix, name, key, value)
         warn(string.format("couldn't change %s's %s to be %s", tostring(name), tostring(key), tostring(value)))
         return false
     end
-    printc(100,255,100,255, string.format("changed command %s's %s at prefix %s", name, key, prefix))
+    printc(100,255,255,255, string.format("changed command %s's %s at prefix %s", name, key, prefix))
     return true
 end
 
@@ -98,12 +107,12 @@ local function run_command(str)
 
     -- Extract the command name and parameters
     local command_name = cmd[1]
-    print(command_name)
     table.remove(cmd, 1)
 
     -- Check if the command exists
     if not prefix.command_list[command_name] then
         error("Command '" .. command_name .. "' not found.")
+        return
     end
 
     local parameters = {}
@@ -131,7 +140,7 @@ local function run_command(str)
             break
         elseif tostring(v) == "table" then
             if string.sub(cmd[1], 1, 1) ~= "{" then
-                error("This parameter needs to start with a {, but it doesn't")
+                error("This parameter needs to start with a \"{\", but it doesn't")
                 break
             end
             local concated = table.concat(cmd, " ")
@@ -155,7 +164,7 @@ end
 
 local function unload()
     prefixes = nil
-    package.loaded.consolelib = nil
+    package.loaded.console = nil
 end
 
 local lib = {}
@@ -168,45 +177,47 @@ lib.unload = unload
 
 callbacks.Unregister("SendStringCmd", "console-lib")
 callbacks.Register("SendStringCmd", "console_lib", run_command)
-printc(100,255,100,255, string.format("Console lib %.1f loaded", lib.version))
 
 printc(255,255,255,255, "Adding 'con' prefix and default commands")
 create_prefix("con")
 
-create_command("con", "create-command")
-change_command("con", "create-command", keys.required_parameters, {prefix = "string", cmd_name = "string"})
-change_command("con", "create-command", keys.callback, function(params)
+local create_command_cmd = create_command("con", "create-command")
+create_command_cmd.required_parameters = {prefix = "string", cmd_name = "string"}
+create_command_cmd.callback = function (params)
     create_command(params.prefix, params.cmd_name)
-end)
+end
 
-create_command("con", "destroy-command")
-change_command("con", "destroy-command", keys.required_parameters, {prefix = "string", cmd_name = "string"})
-change_command("con", "destroy-command", keys.callback, function(params)
+local destroy_command_cmd = create_command("con", "destroy-command")
+destroy_command_cmd.required_parameters = {prefix = "string", cmd_name = "string"}
+destroy_command_cmd.callback = function(params)
     destroy_command(params.prefix, params.cmd_name)
-end)
+end
 
-create_command("con", "change-command-callback")
-change_command("con", "change-command-callback", keys.required_parameters, {prefix = "string", cmd_name = "string", callback = "function"})
-change_command("con", "change-command-callback", keys.callback, function(params)
+local change_command_callback = create_command("con", "change-command-callback")
+change_command_callback.required_parameters = {prefix = "string", cmd_name = "string", callback = "function"}
+change_command_callback.callback = function(params)
     change_command(params.prefix, params.cmd_name, keys.callback, params.callback)
-end)
+end
 
-create_command("con", "change-command-parameters")
-change_command("con", "change-command-parameters", keys.required_parameters, {prefix = "string", cmd_name = "string", new_params = "table"})
-change_command("con", "change-command-parameters", keys.callback, function(params)
+local change_command_parameters = create_command("con", "change-command-parameters")
+change_command_parameters.required_parameters = {prefix = "string", cmd_name = "string", new_params = "table"}
+change_command_parameters.callback = function(params)
     change_command(params.prefix, params.cmd_name, keys.required_parameters, params.new_params)
-end)
+end
 
-create_command("con", "create-prefix")
-change_command("con", "create-prefix", keys.required_parameters, {new_prefix = "string"})
-change_command("con", "create-prefix", keys.callback, function(params)
+local create_prefix_command = create_command("con", "create-prefix")
+create_prefix_command.required_parameters = {new_prefix = "string"}
+create_prefix_command.callback = function(params)
     create_prefix(params.new_prefix)
-end)
+end
 
-create_command("con", "command-exist")
-change_command("con", "command-exist", keys.required_parameters, {prefix = "string", cmd_name = "string"})
-change_command("con", "command-exist", keys.callback, function(params)
+local command_exist_command = create_command("con", "command-exist")
+command_exist_command.required_parameters = {prefix = "string", cmd_name = "string"}
+command_exist_command.callback = function(params)
     print(command_exists(params.prefix, params.cmd_name))
-end)
+end
 
-return lib
+printc(100,255,100,255, string.format("Console lib %.1f loaded", lib.version))
+if not debug then
+    return lib
+end
